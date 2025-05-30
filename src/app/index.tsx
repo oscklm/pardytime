5e586d8b9ec5a769fdae6156593507aae1e1be95
Oscar Landmark <oscar@oneguylabs.com>
2025-05-30 21:35:29 +0200

    Move asset folder, add touchablebounce

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
 import { api } from "@/convex/_generated/api";
+import TouchableBounce from "@/shared/ui/TouchableBounce";
 import { useQuery } from "convex/react";
 import { Text, View } from "react-native";
 import { StyleSheet } from "react-native-unistyles";
@@ -7,7 +8,25 @@ export default function Index() {
   const boards = useQuery(api.boards.queries.getAll);
   return (
     <View style={styles.container}>
-      {boards?.map(({ _id, title }) => <Text key={_id}>{title}</Text>)}
+      <View style={{ marginBottom: 20 }}>
+        {boards?.map(({ _id, title }) => <Text key={_id}>{title}</Text>)}
+      </View>
+      <TouchableBounce
+        onPress={() => {
+          console.log("Button pressed");
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "lightblue",
+            paddingHorizontal: 25,
+            paddingVertical: 20,
+            borderRadius: 5,
+          }}
+        >
+          <Text style={{ color: "black" }}>Press Me</Text>
+        </View>
+      </TouchableBounce>
     </View>
   );
 }
