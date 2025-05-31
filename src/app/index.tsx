72853d2c7a91ea4bac26557d3796e579f170bc1a
Oscar Landmark <oscar@oneguylabs.com>
2025-05-31 14:06:21 +0200

    Refactor index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,35 +1,43 @@
-import { Text, View } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 import { StyleSheet } from "react-native-unistyles";
 import ColorPreview from "@/shared/components/ColorPreview";
 import Button from "@/shared/ui/Button";
 
 export default function Index() {
 	return (
-		<View style={styles.container}>
-			<View>
-				<Text style={{ fontWeight: "600", marginBottom: 6 }}>Theme colors</Text>
-				<View style={styles.colorsContainer}>
-					<ColorPreview percent={60} color="hsl(252, 87.90%, 42.00%)" />
-					<ColorPreview percent={30} color="hsl(158, 74.00%, 60.80%)" />
-					<ColorPreview percent={10} color="hsl(252, 36.50%, 50.60%)" />
+		<SafeAreaView style={{ flex: 1, backgroundColor: "hsl(44, 9%, 69%)" }}>
+			<View style={styles.container}>
+				<View>
+					<Text style={{ fontWeight: "600", marginBottom: 6 }}>
+						Theme colors
+					</Text>
+					<View style={styles.colorsContainer}>
+						<ColorPreview percent={60} color="hsl(252, 87.90%, 42.00%)" />
+						<ColorPreview percent={30} color="hsl(158, 74.00%, 60.80%)" />
+						<ColorPreview percent={10} color="hsl(252, 36.50%, 50.60%)" />
+					</View>
+				</View>
+				<View style={{ width: "100%" }}>
+					<Button
+						label="Click on me"
+						onPress={() => alert("Button Pressed!")}
+					/>
 				</View>
 			</View>
-			<Button label="Click on me" onPress={() => alert("Button Pressed!")} />
-		</View>
+		</SafeAreaView>
 	);
 }
 
 const styles = StyleSheet.create(() => ({
 	container: {
 		flex: 1,
-		justifyContent: "center",
+		justifyContent: "space-between",
 		alignItems: "center",
-		backgroundColor: "hsl(44, 9%, 69%)",
 		padding: 20,
 		gap: 20,
 	},
 	colorsContainer: {
+		width: "100%",
 		flexDirection: "row",
-		width: 200,
 	},
 }));
