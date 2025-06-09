1c0de1f561ff848c5f2acc793d5d7dcc06e89978
Oscar Landmark <oscar@oneguylabs.com>
2025-06-09 12:11:27 +0200

    Create AuthLoaded.tsx

---
+++ src/components/AuthLoaded.tsx
@@ -0,0 +1,11 @@
+import { useConvexAuth } from "convex/react";
+import LoadingView from "./LoadingView";
+
+export const AuthLoaded = ({ children }: React.PropsWithChildren<unknown>) => {
+  const { isLoading } = useConvexAuth();
+
+  if (isLoading) {
+    return <LoadingView />;
+  }
+  return children;
+};
