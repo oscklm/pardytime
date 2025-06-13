6c8fadb52c0351a81202f75d164a53b20962df6a
Oscar Landmark <oscar@oneguylabs.com>
2025-06-13 15:55:35 +0200

    Update app version to 1.0.3 in app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -32,7 +32,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: getAppName(),
   slug: "jeopardytime",
-  version: "1.0.2",
+  version: "1.0.3",
   orientation: "portrait",
   icon: "./src/assets/images/icon.png",
   scheme: "jeopardytime",
